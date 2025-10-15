ad2b603d6982fc0ecdfce743851743f012a68ae7
NicholasDuBois <evingerhorowitz@gmail.com>
2025-10-15 11:35:40 +0800

    Add contract addresses

---
+++ frontend/abi/FHESalaryAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const FHESalaryAddresses = { 
+  "11155111": { address: "0x2E4F4BE204aA47bf7FD99BCD678BfC81546851Aa", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x0000000000000000000000000000000000000000", chainId: 31337, chainName: "hardhat" },
+};
